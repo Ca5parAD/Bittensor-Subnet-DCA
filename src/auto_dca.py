47db96b5e90e6c8a4678d7e679bb41a39b6323e6
Ca5parAD <github.unearned388@passmail.net>
2025-07-05 22:07:31 +0100

    Various - needs checking

--- src/auto_dca.py
+++ src/auto_dca.py
@@ -17,62 +17,45 @@ netuids_to_stake = []
 
 # **********************************
 
-import bittensor as bt
-from bittensor.utils.balance import Balance
+import bittensor
+import auto_dca_utils as custom
 
 # Setup wallet
-wallet = bt.wallet(path=your_wallet_path, name=your_wallet_name)
-subtensor = bt.subtensor(network=your_network)
+wallet = bittensor.wallet(path=your_wallet_path, name=your_wallet_name)
+subtensor = bittensor.subtensor(network=your_network)
 
-hotkeys_to_stake = []
-amounts_to_stake = []
+wallet_info = custom.FullWalletInfo(wallet, subtensor)
 
+print(f'\nFor coldkey: {wallet.coldkeypub.ss58_address}')
+print(f'Free tao: {wallet_info.free_tao} | Root stake: {wallet_info.root_stake} | Alpha stake: {wallet_info.alpha_stake}')
+print(f'Total: {wallet_info.free_tao + wallet_info.root_stake + wallet_info.alpha_stake}')
 
-# Show balance and subnets to stake into
-wallet_balance = subtensor.get_balance(address=wallet.coldkeypub.ss58_address)
-print(f'Your wallet balance is: {wallet_balance}\n')
-
-print(f'You would like to stake into the following {len(netuids_to_stake)} subnets:')
+print(f'\nYou would like to stake into the following {len(netuids_to_stake)} subnets:')
 for i in range(len(netuids_to_stake)):
     print(netuids_to_stake[i])
+print(f'Requiring {bittensor.Balance(stake_amount*len(netuids_to_stake))} free')
 
-continue_check = str(input('Correct? (y/n): '))
+continue_check = str(input('\nCorrect? (y/n): '))
 if not (continue_check == 'y' or continue_check == 'Y'):
     exit()
 
-# Check required balance for stake
-print(f'Requiring {stake_amount*len(netuids_to_stake)}τ free')
+wallet_info.check_balances(stake_amount*len(netuids_to_stake))
 
-if stake_amount*len(netuids_to_stake) > wallet_balance:
-    print('Tao balance insufficient')
-    exit()
-else:
-    print('\n')
+wallet_info.organise_hotkeys_to_stake(netuids_to_stake)
 
 
-# Get delegation data a sort by subnet number
-delegated_info = subtensor.get_delegated(coldkey_ss58=wallet.coldkeypub.ss58_address)
-delegated_info.sort(key=lambda info: info.netuid)  # Sort by subnet ID
-
-no_stake_flag = False
-for netuid in netuids_to_stake:
-    for i in range(len(delegated_info)):
-        if netuid == delegated_info[i].netuid:
-            print(f'Subnet {netuid} staked to: {delegated_info[i].hotkey_ss58}')
-            hotkeys_to_stake.append(delegated_info[i].hotkey_ss58)
-            amounts_to_stake.append(Balance.from_tao(stake_amount))
-            break
-        elif i == len(delegated_info) - 1:
-            print(f'No stake for subnet: {netuid}')
-            no_stake_flag = True
 print('\n')
-
-if no_stake_flag == True:
+if wallet_info.no_stake_flag == True:
     print('Need to make an initial stake on each subnet before using this program')
     exit()
+else:
+    continue_check = str(input('Correct? (y/n): '))
+    if not (continue_check == 'y' or continue_check == 'Y'):
+        exit()
 
 subtensor.add_stake_multiple(wallet=wallet, netuids=netuids_to_stake, hotkey_ss58s=hotkeys_to_stake, amounts=amounts_to_stake)
 print('Your stakes have been made!')
 
-wallet_balance = subtensor.get_balance(address=wallet.coldkeypub.ss58_address)
-print(f'Your wallet balance is now: {wallet_balance}')
+wallet_balance = wallet_info.__init__(wallet, subtensor)
+print(f'Free tao: {wallet_info.free_tao} | Root stake: {wallet_info.root_stake} | Alpha stake: {wallet_info.alpha_stake}')
+print(f'Total: {wallet_info.free_tao + wallet_info.root_stake + wallet_info.alpha_stake}')
---
+++ src/auto_dca_utils.py
@@ -0,0 +1,53 @@
+import bittensor
+
+class FullWalletInfo:
+    def __init__(self, wallet, subtensor):
+        self.wallet = wallet
+        self.subtensor = subtensor
+        self.free_tao = self.subtensor.get_balance(address=self.wallet.coldkeypub.ss58_address) # Gets free tao balance
+        
+        self.delegated_info = self.subtensor.get_delegated(coldkey_ss58=self.wallet.coldkeypub.ss58_address)
+        self.delegated_info.sort(key=lambda info: info.netuid)  # Sort by subnet ID
+
+        self.root_stake = 0
+        self.alpha_stake = 0
+        for info in self.delegated_info:
+            if info.netuid == 0:
+                self.root_stake += info.stake # Sums root stake
+
+            else:
+                subnet_info = self.subtensor.subnet(netuid=info.netuid)
+                self.alpha_stake += bittensor.Balance(float(info.stake) * float(subnet_info.price)) # Converts alpha stake to tao and sums
+
+
+    def check_balances(self, total_stake_amount):
+        print(total_stake_amount)
+        if total_stake_amount < float(self.free_tao):
+            pass
+        elif total_stake_amount < float(self.free_tao) + float(self.root_stake):
+            root_unstake_needed = total_stake_amount - self.free_tao
+            print('Not enough free tao')
+            continue_check = str(input(f'Would you like to unstake {root_unstake_needed} from root? (y/n): '))
+            if (continue_check == 'y' or continue_check == 'Y'):
+                self.subtensor.unstake(wallet=self.wallet, netuid=0, amount=root_unstake_needed)
+            else:
+                exit()
+        else:
+            print(f'Free tao and root stake insufficient to make stake')
+            exit()
+                
+    def organise_hotkeys_to_stake(self, netuids_to_stake):
+        self.hotkeys_to_stake = []
+        self.no_stake_flag = False
+
+        for netuid in netuids_to_stake:
+            for i in range(len(self.delegated_info)):
+                if netuid == self.delegated_info[i].netuid:
+                    print(f'Subnet {netuid} staked to: {self.delegated_info[i].hotkey_ss58}')
+                    self.hotkeys_to_stake.append(self.delegated_info[i].hotkey_ss58)
+                    break
+                elif i == len(self.delegated_info) - 1:
+                    print(f'No stake for subnet: {netuid}')
+                    self.no_stake_flag = True
+        
+
