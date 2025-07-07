b055a3e4c90d789ee6ee5aafadd55d03e4c501b9
Ca5parAD <github.unearned388@passmail.net>
2025-07-07 19:45:52 +0100

    Sort token objects and floats

--- src/auto_dca_utils.py
+++ src/auto_dca_utils.py
@@ -4,7 +4,7 @@ class FullWalletInfo:
     def __init__(self, wallet, subtensor):
         self.wallet = wallet
         self.subtensor = subtensor
-        self.free_tao = self.subtensor.get_balance(address=self.wallet.coldkeypub.ss58_address) # Gets free tao balance
+        self.free_tao = float(self.subtensor.get_balance(address=self.wallet.coldkeypub.ss58_address)) # Gets free tao balance
         
         self.delegated_info = self.subtensor.get_delegated(coldkey_ss58=self.wallet.coldkeypub.ss58_address)
         self.delegated_info.sort(key=lambda info: info.netuid)  # Sort by netuid
@@ -13,16 +13,16 @@ class FullWalletInfo:
         self.alpha_stake = 0
         for info in self.delegated_info:
             if info.netuid == 0:
-                self.root_stake += info.stake # Sums root stake
+                self.root_stake += float(info.stake) # Sums root stake
 
             else:
                 subnet_info = self.subtensor.subnet(netuid=info.netuid)
-                self.alpha_stake += bittensor.Balance(float(info.stake) * float(subnet_info.price)) # Converts alpha stake to tao and sums
+                self.alpha_stake += float(info.stake) * float(subnet_info.price) # Converts alpha stake to tao and sums
 
     def print_balances(self):
         print(f'\nFor coldkey: {self.wallet.coldkeypub.ss58_address}')
-        print(f'Free tao: {self.free_tao} | Root stake: {self.root_stake} | Alpha stake: {self.alpha_stake}')
-        print(f'Total: {self.free_tao + self.root_stake + self.alpha_stake}')
+        print(f'Free tao: τ{self.free_tao} | Root stake: τ{self.root_stake} | Alpha stake: τ{self.alpha_stake}')
+        print(f'Total: τ{self.free_tao + self.root_stake + self.alpha_stake}')
 
     def check_netuids_to_stake(self, netuids_to_stake, stake_amount):
         self.netuids_to_stake = netuids_to_stake
@@ -30,15 +30,15 @@ class FullWalletInfo:
         print(f'\nYou would like to stake into the following {len(self.netuids_to_stake)} subnets:')
         for i in range(len(self.netuids_to_stake)):
             print(self.netuids_to_stake[i])
-        print(f'Requiring {bittensor.Balance(self.stake_amount*len(self.netuids_to_stake))}')
+        print(f'Requiring τ{self.stake_amount*len(self.netuids_to_stake)}')
 
     def check_balances_for_stake(self, total_stake_amount):
-        if total_stake_amount < float(self.free_tao):
+        if total_stake_amount < self.free_tao:
             pass
 
         # If total stake amount < free tao and root stake, unstake remander from root
-        elif total_stake_amount < float(self.free_tao) + float(self.root_stake):
-            root_unstake_needed = bittensor.Balance(float(total_stake_amount) - float(self.free_tao))
+        elif total_stake_amount < self.free_tao + self.root_stake:
+            root_unstake_needed = total_stake_amount - self.free_tao
             print('Not enough free tao')
             continue_check(f'Would you like to unstake {root_unstake_needed} from root?')
 
